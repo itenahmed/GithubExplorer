da3052857ce5dbb9bd927b2f1cd724ac9887dfb0
itenahmed <itenahmed00@yahoo.com>
2025-10-18 20:04:24 +0300

    Enable eslint.ignoreDuringBuilds for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // allow deploy even if lint errors exist
+  },
 };
 
 export default nextConfig;
